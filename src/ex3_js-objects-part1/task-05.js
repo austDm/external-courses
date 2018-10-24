50ed730230d540c0f4abacf9ce5a092fd1269c2a
austDm <aust.dm.epam@gmail.com>
2018-10-24 09:20:35 +0300

    objects_1

---
+++ src/ex3_js-objects-part1/task-05.js
@@ -0,0 +1,13 @@
+'use strict'
+function copyObj(ob) {
+  var copyOb = {};
+  var key; 
+    
+  for (key in ob) {
+      
+      copyOb[key] = ob[key];
+  }
+    
+  return copyOb;
+}
+module.exports = copyObj; 
