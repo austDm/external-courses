91fb50d6739a10522783fe96eb7869adecf2f028
austDm <aust.dm.epam@gmail.com>
2018-11-29 12:49:20 +0300

    Fix 29.11

--- src/ex15_js-oop/Presentation_HW/task.js
+++ src/ex15_js-oop/Presentation_HW/task.js
@@ -1,60 +1,58 @@
-'use strict';
-var shape = {
-  type:'Shape',
-  getType: function () {
-    return shape.type;
-  },
-  getPerimeter: function () {
-    var per = 0;
-    var i;
-    for (i = 0; i < this.sides.length; i++) {
-      per += this.sides[i];
+(function () {
+  'use strict';
+  var shape = {
+    getType: function () {
+      return this.type;
+    },
+    getPerimeter: function () {
+      return Object.keys(this).filter(prop => prop.match(/^[a-z]{1}$/i))
+      .reduce((per, side) => { return per += this[side]; }, 0);
+    },
+    draw: function () {
+      console.log(this.name + ' is drawn!');
     }
-    return per;
-  },
-  draw: function () {
-    console.log(this.name + ' is drawn!');
+  };
+  
+  function Triangle(a, b, c, name){
+    this.name = name;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.type = 'Triangle';
   }
-};
-
-function Triangle(a, b, c, name){
-  this.name = name;
-  this.a = a;
-  this.b = b;
-  this.c = c;
-  this.sides = Array.from(arguments).slice(0, 3);
-}
-
-function Square(a, name){
-  this.name = name;
-  this.a = this.b = this.c = this.d = a;
-  this.sides = [a,a,a,a];
-}
-
-function Rhombus(a, rad, name){
-  Square.apply(this, arguments);
-  this.name = name;
-  this.rad = rad;
-}
-
-Triangle.prototype = shape;
-Square.prototype = shape;
-Rhombus.prototype = Object.create(Square.prototype);
-Rhombus.prototype.area = function () {
-  return (this.a * this.a * Math.sin(this.rad)).toFixed(1);
-};
-
-var tri = new Triangle (1, 2, 3, 'tri');
-var squ = new Square (1, 'squ');
-var rho = new Rhombus (1, 1.5708, 'rho');
-
-console.log(tri.getType());
-console.log(tri.getPerimeter());
-tri.draw();
-console.log(squ.getType());
-console.log(squ.getPerimeter());
-squ.draw();
-console.log(rho.getType());
-console.log(rho.getPerimeter());
-console.log(rho.area());
-rho.draw();
+  
+  function Square(a, name){
+    this.name = name;
+    this.a = this.b = this.c = this.d = a;
+    this.type = 'Square';
+  }
+  
+  function Rhombus(a, rad, name){
+    Square.apply(this, arguments);
+    this.name = name;
+    this.rad = rad;
+    this.type = 'Rhombus';
+  }
+  
+  Triangle.prototype = shape;
+  Square.prototype = shape;
+  Rhombus.prototype = Object.create(Square.prototype);
+  Rhombus.prototype.area = function () {
+    return (this.a * this.a * Math.sin(this.rad)).toFixed(1);
+  };
+  
+  var tri = new Triangle (1, 2, 3, 'tri');
+  var squ = new Square (1, 'squ');
+  var rho = new Rhombus (1, 1.5708, 'rho');
+  
+  console.log(tri.getType());
+  console.log(tri.getPerimeter());
+  tri.draw();
+  console.log(squ.getType());
+  console.log(squ.getPerimeter());
+  squ.draw();
+  console.log(rho.getType());
+  console.log(rho.getPerimeter());
+  console.log(rho.area());
+  rho.draw();
+}());
